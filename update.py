01d32f4e3bfcfdb9d6ac1033b905dc5a55cf3910
anomalia <anavgo@gmail.com>
2024-04-26 10:29:11 +0200

    fixes first obs

--- update.py
+++ update.py
@@ -277,23 +277,25 @@ if __name__ == "__main__":
     obs = get_obs(id_project=main_project_bdc)
     if len(obs) > 0:
         df_obs, df_photos = get_dfs(obs)
-        df_obs["taxon_id"] = df_obs["taxon_id"].astype(int)
+        try:
+            df_obs["taxon_id"] = df_obs["taxon_id"].astype(int)
 
-        # Completar campos de taxonomías
-        cols = ["class", "order", "family", "genus"]
-        for col in cols:
-            df_obs.loc[df_obs[col].isnull(), col] = df_obs[df_obs[col].isnull()][
-                "taxon_id"
-            ].apply(lambda x: get_missing_taxon(x, col))
+            # Completar campos de taxonomías
+            cols = ["class", "order", "family", "genus"]
+            for col in cols:
+                df_obs.loc[df_obs[col].isnull(), col] = df_obs[df_obs[col].isnull()][
+                    "taxon_id"
+                ].apply(lambda x: get_missing_taxon(x, col))
 
-        # Sacar columna marino
-        taxon_url = "https://raw.githubusercontent.com/eosc-cos4cloud/mecoda-orange/master/mecoda_orange/data/taxon_tree_with_marines.csv"
-        taxon_tree = pd.read_csv(taxon_url)
+            # Sacar columna marino
+            taxon_url = "https://raw.githubusercontent.com/eosc-cos4cloud/mecoda-orange/master/mecoda_orange/data/taxon_tree_with_marines.csv"
+            taxon_tree = pd.read_csv(taxon_url)
 
-        df_obs = pd.merge(
-            df_obs, taxon_tree[["taxon_id", "marine"]], on="taxon_id", how="left"
-        )
-
+            df_obs = pd.merge(
+                df_obs, taxon_tree[["taxon_id", "marine"]], on="taxon_id", how="left"
+            )
+        except:
+            pass
         df_obs.to_csv(f"data/{main_project_bdc}_obs.csv", index=False)
         df_photos.to_csv(f"data/{main_project_bdc}_photos.csv", index=False)
 
@@ -302,8 +304,12 @@ if __name__ == "__main__":
         df_users.to_csv(f"data/{main_project_bdc}_users.csv", index=False)
 
         # Dataframe de marino/terrestre
-        df_marine = get_marine_df(df_obs)
-        df_marine.to_csv(f"data/{main_project_bdc}_marines.csv", index=False)
+        try:
+            df_marine = get_marine_df(df_obs)
+            df_marine.to_csv(f"data/{main_project_bdc}_marines.csv", index=False)
+        except:
+            df_obs["marine"] = None
+            df_obs.to_csv(f"data/{main_project_bdc}_marines.csv", index=False)
 
     # Dataframe métricas totales
     total_species, total_participants, total_obs = get_main_metrics(main_project_bdc)
